e6e7c80b20bb6a46fed2cbf4996341c7308e3a28
Yasser hassan ikuzwe mugisha <1099664@pdsb.net>
2026-07-22 00:18:59 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,14 @@
 const { Events } = require('discord.js');
 const { startScheduleRunner } = require('../utils/scheduleRunner');
+const { restoreGiveaways } = require('../commands/utility/giveaway');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
         client.user.setActivity('YSER Flow | /help', { type: 3 });
         startScheduleRunner(client);
+        await restoreGiveaways(client).catch(err => console.error('[GIVEAWAY RESTORE]', err));
     },
 };
