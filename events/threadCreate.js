bd69f01beea1af3e4d6e545937dd09b32a54896d
Yasser hassan ikuzwe mugisha <ysertrades@gmail.com>
2026-09-22 13:34:35 -0400

    XP v2: leveling engine + message/thread hooks + feature toggle

--- events/threadCreate.js
+++ events/threadCreate.js
@@ -1,3 +1,19 @@
 'use strict';
+
 const { Events } = require('discord.js');
-module.exports = { name: Events.ThreadCreate, async execute() {} };
+const { isFeatureEnabled } = require('../utils/featureToggles');
+
+module.exports = {
+  name: Events.ThreadCreate,
+  async execute(thread) {
+    try {
+      const guildId = thread.guild?.id;
+      if (!guildId) return;
+      if (!isFeatureEnabled(guildId, 'leveling')) return;
+      const leveling = require('../utils/levelingEngine');
+      leveling.handleThreadCreate(thread);
+    } catch (err) {
+      console.error('[XP threadCreate]', err);
+    }
+  },
+};
