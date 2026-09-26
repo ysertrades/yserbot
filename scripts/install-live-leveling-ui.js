e27c4270dd315cb7392899dd87b2710da4edecb1
Yasser hassan ikuzwe mugisha <ysertrades@gmail.com>
2026-09-25 22:56:19 -0400

    feat(leveling): install script for live XP ladder UI (gzip embed)

---
+++ scripts/install-live-leveling-ui.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+console.log('Run from repo after pulling - content will be filled');
+process.exit(1);
